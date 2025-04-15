cb334c4eb935a492cbc98e5e3e06ce972fd1edf7
blistalicious <nihadibrahimli2@gmail.com>
2025-04-15 04:20:18 +0400

    improved js code for pfp overlay

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const body = document.body;
 
 pfp.addEventListener("click", function () {
   const div = document.createElement("div");
-  div.style.position = "absolute";
+  div.style.position = "fixed";
   div.style.backgroundColor = "rgba(0, 0, 0, 0.85)";
   div.style.width = "100%";
   div.style.height = "100%";
   div.style.top = "0";
   div.style.left = "0";
+  div.style.zIndex = "9999";
 
-  const img = document.createElement("img");
-  img.src = pfp.src;
+  const img = pfp.cloneNode(true);
+  img.removeAttribute("id");
   img.style.width = "12.5rem";
   img.style.height = "12.5rem";
   img.style.borderRadius = "100%";
@@ -20,13 +21,10 @@ pfp.addEventListener("click", function () {
   img.style.left = "50%";
   img.style.transform = "translate(-50%, -50%)";
 
-  body.style.position = "relative";
-
   document.body.appendChild(div);
   div.appendChild(img);
 
   div.addEventListener("click", function () {
     div.remove();
-    body.style.position = "static";
   });
 });
